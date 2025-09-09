55cc00f53786971c99c41c98bcc20df43fc4f416
roxy <roxybell.alvarez@correoaiep.cl>
2025-09-09 19:53:28 -0300

    Modificacion del inicio

---
+++ app/(tabs)/Inicio/Prod_Lavadora.tsx
@@ -0,0 +1,82 @@
+import { ThemedText } from "@/components/ThemedText";
+import { StyleSheet, Text, TouchableOpacity, View, ScrollView } from 'react-native';
+
+const Prod_Lavadora = () => {
+    return (
+        <View style={styles.mainContainer}>
+            <ScrollView contentContainerStyle={styles.scrollContent}>
+                <ThemedText style={styles.title}>Lavadora LG 12 kilos</ThemedText>
+                <ThemedText style={styles.subtitle}>Tienda:</ThemedText>
+                <ThemedText style={styles.paragraph}>
+                    Falabella - Ñuñoa
+                </ThemedText>
+                <ThemedText style={styles.subtitle}>Fecha:</ThemedText>
+                <ThemedText style={styles.paragraph}>
+                    09/09/2025
+                </ThemedText>
+            </ScrollView>
+
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Eliminar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.button}>
+                    <Text style={styles.buttonText}>Editar</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: '#fff',
+    },
+    scrollContent: {
+        padding: 24,
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 16,
+        color: '#222',
+    },
+    subtitle: {
+        fontSize: 20,
+        fontWeight: '600',
+        marginTop: 20,
+        marginBottom: 8,
+        color: '#333',
+    },
+    paragraph: {
+        fontSize: 16,
+        marginBottom: 12,
+        color: '#444',
+        lineHeight: 22,
+    },
+    buttonContainer: {
+        flexDirection: "row",
+        marginBottom: 40,
+        marginHorizontal: 40,
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: 16,
+        borderTopWidth: 1,
+        borderTopColor: '#f0f0f0',
+    },
+    button: {
+        flex: 1,
+        backgroundColor: "#2563eb",
+        paddingVertical: 12,
+        borderRadius: 8,
+        marginHorizontal: 8,
+        alignItems: "center",
+    },
+    buttonText: {
+        color: "#fff",
+        fontWeight: 'bold',
+    },
+});
+
+export default Prod_Lavadora;
---
+++ app/(tabs)/Inicio/index.tsx
@@ -0,0 +1,88 @@
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
+import React from 'react';
+import {  StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { ThemedText } from "@/components/ThemedText";
+import { useRouter } from 'expo-router';
+
+const inicio = () => {
+   const router = useRouter();
+
+   return (
+       <View style={{ flex: 1, alignItems: "center" }}>
+       <ThemedText type="title" style={{ fontSize: 20, textAlign: "center", marginTop: 40 }}>
+         Tus Productos
+       </ThemedText>
+
+       {/* Trajetas de productos */}
+       <View style={styles.cardsContainer}>
+        <TouchableOpacity style={styles.card} testID='tarjeta-producto-auto' onPress={() => router.push('/(tabs)/Inicio/Prod_Auto')}>
+         <View style={styles.cardContent}>
+         <MaterialCommunityIcons name="car" size={24} color="#1976d2" />
+         <Text style={styles.cardText}>Auto jeep wrangler</Text>
+      </View>
+        <Ionicons name="chevron-forward" size={24} color="#1976d2" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.card} testID='tarjeta-producto-lavadora' onPress={() => router.push('/(tabs)/Inicio/Prod_Lavadora')}>
+        <View style={styles.cardContent}>
+         <MaterialCommunityIcons name="washing-machine" size={24} color="#1976d2" />
+         <Text style={styles.cardText}>Lavadora LG 12 kilos</Text>
+        </View>
+       <Ionicons name="chevron-forward" size={24} color="#1976d2" />
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.card} testID='tarjeta-producto-microondas' onPress={() => router.push('/(tabs)/Inicio/Prod_Microondas')}>
+        <View style={styles.cardContent}>
+         <MaterialCommunityIcons name="microwave" size={24} color="#1976d2" />
+         <Text style={styles.cardText}>Microondas Samsung</Text>
+        </View>
+       <Ionicons name="chevron-forward" size={24} color="#1976d2" />
+      </TouchableOpacity>
+   </View>
+ </View>
+ );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#cecacaff',
+      padding: 24,
+      alignItems: 'center',
+    },
+
+    cardsContainer: {
+     width: '100%',
+     gap: 16,
+     },
+
+     card: {
+      backgroundColor: '#ffffffff',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+      paddingVertical: 18,
+      paddingHorizontal: 20,
+      borderRadius: 12,
+      marginBottom: 8,
+      shadowColor: '#000',
+      shadowOpacity: 0.04,
+      shadowRadius: 6,
+      shadowOffset: { width: 0, height: 1 },
+      elevation: 1,
+ },
+
+    cardContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 10,
+    },
+    
+     cardText: {
+       color: '#222',
+       fontSize: 18,
+       fontWeight: '600',
+   },
+});
+
+export default inicio;
