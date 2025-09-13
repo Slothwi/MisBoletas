95c0230cd32dea289eb8dae90a582821b68a44df
liritb <lirit.brignardello@correoaiep.cl>
2025-09-13 03:56:06 -0300

    Revert "layout?"
    This reverts commit 54780dc0450ddf6cfaef8d0a186932b44c0e156e.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,6 +7,7 @@ import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import AntDesign from '@expo/vector-icons/AntDesign';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -37,7 +38,7 @@ export default function TabLayout() {
         name="configuracion"
         options={{
           title: 'Configuración',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarIcon: ({ color }) => <AntDesign name="setting" size={24} color="black" />,
         }}
       />
     </Tabs>
