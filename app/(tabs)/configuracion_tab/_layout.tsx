f2daff2cf11012dce78d0a44c10437a884fc5da3
conigon <constanza.gonzalemed@correoaiep.cl>
2025-10-06 20:29:26 -0300

    Eliminación Fonts de la carpeta assets y se agrego el icono de el inicio de la app

--- app/(tabs)/configuracion_tab/_layout.tsx
+++ app/(tabs)/configuracion_tab/_layout.tsx
@@ -1,24 +1,11 @@
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ActivityIndicator, View } from 'react-native';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" />
-      </View>
-    );
-  }
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
