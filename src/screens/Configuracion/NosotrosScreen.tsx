c33fc81b207f6031613b51fef80e509cd8eb85f2
conigon <constanza.gonzalemed@correoaiep.cl>
2025-12-15 18:54:48 -0300

    Nosotros y logos añadidos

--- src/screens/Configuracion/NosotrosScreen.tsx
+++ src/screens/Configuracion/NosotrosScreen.tsx
@@ -26,16 +26,19 @@ const NosotrosScreen = () => {
       <ScrollView style={{ width: '100%' }} contentContainerStyle={{ paddingBottom: 40 }}>
         
         <View style={[cards.base, { backgroundColor: cardBg }]}>
-          <ThemedText type='subtitle'>¡Hola! Somos el equipo.</ThemedText>
+          <ThemedText type='subtitle'>¡Hola! Somos el equipo de MisBoletas.</ThemedText>
           <ThemedText style={{ marginTop: 8 }}>
-            Este es nuestro primer proyecto conjunto como desarrolladores de aplicaciones móviles.
+            Este es nuestro primer proyecto conjunto como desarrolladores de aplicaciones móviles y estamos muy felices de que lo tengas en tus manos.
+            Somos cinco personas movidas por dar soluciones prácticas a problemas cotidianos a través de la tecnología.
           </ThemedText>
         </View>
         
         <View style={[cards.base, { backgroundColor: cardBg }]}>
-          <ThemedText type='subtitle'>Nuestra Misión</ThemedText>
+          <ThemedText type='subtitle'>¿Por qué creamos MisBoletas</ThemedText>
           <ThemedText style={{ marginTop: 8 }}>
-            Empoderar a los consumidores para que no pierdan sus garantías.
+            Nos hemos enfrentado a situaciones que son comunes para muchos consumidores: perder boletas de compra, no tener acceso rápido a ellas cuando 
+            las necesitamos para devoluciones o garantías, y la dificultad de organizar y almacenar estos documentos importantes. 
+            Hemos desarrollado esta herramienta para resolver estos problemas y hacer que la gestión de tus boletas sea más sencilla y eficiente. Hemos desarrollado esta herramienta para ayudarte a organizar tus productos.
           </ThemedText>
         </View>
       </ScrollView>
